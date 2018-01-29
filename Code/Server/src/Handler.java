fa3c3ef1c58af9dc30d6abaf2b48c28ee75b2851
Marcel Herhold <mh11muka@studserv.uni-leipzig.de>
2018-01-29 12:49:47 +0100

    Server hochgeladen

--- Code/Server/src/Handler.java
+++ Code/Server/src/Handler.java
@@ -7,100 +7,88 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.HashMap;
-
 import db.PupilDB;
 
-
 /**
+ * Handle the communication between Server, Client and DB.
  * @author marcel
  *
  */
 public class Handler implements Runnable {
-	
-	/**
-	 * Socket für den Client
-	 */
-	private Socket client;
-	private PupilDB db1;
-	
-	/**
-	 * Konstruktor zum Übergeben des Clients
-	 * @param client Client welcher als Thread auf dem Server laufen soll
-	 */
-	
-	public Handler(Socket client,PupilDB db1) {
-		this.client = client;
-		this.db1 = db1;
-	}
-
-	
-	/**
-	 * Verwaltet und andwortet dem Client
-	 */
-	public void run() {
-		try {
-			OutputStream out = client.getOutputStream();
-			PrintWriter writer = new PrintWriter(out);
-			
-			InputStream in = client.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-			String s = " ";
-			
-			HashMap<Integer, Integer> rangliste;
-			s = reader.readLine();
-			int id = 0;
-			while(!s.equals("disconnect")) {
-				
-				if(s.equals("Ueberpruefe ID")) {
-					//s = id
-					s = reader.readLine();
-					System.out.println("Prufen ID");
-					//id an DB schicken
-					id = Integer.parseInt(s);
-					System.out.println(id);
-					boolean inID = db1.isID(id);
-					System.out.println(inID);
-					if(inID) {//prüfen ob id in db
-						writer.write("true" + "\n");
-						writer.flush();
-					}else {
-						writer.write("false\n");
-						writer.flush();
-					}
-				}
-				if(s.equals("Punkte")) {
-					Integer points = db1.getScore(id);
-					System.out.println("Punkte " +points);
-					writer.write(points.toString() + "\n");
-					writer.flush();
-				}
-				if(s.equals("Rangliste")) {
-					rangliste = db1.getToplist();
-					for(int i = 1 ; i<=rangliste.size();i++) {
-						System.out.println(i +". platz" +rangliste.get(i));
-						writer.write(rangliste.get(i) + "\n");
-						writer.flush();
-					}
-					writer.write(db1.getRank(id) + "\n");
-					writer.flush();	
-				}
-				
-				System.out.println("ich lese ");
-				
-				s = reader.readLine();
-				System.out.println("Empfangen vom Client( " +client.getPort() + "): " +s);
-			}
-			System.out.println("Client aus");
-			Server.deleteSocket(client);
-			writer.close();
-			reader.close();
-			client.close();
-		}catch(SocketException e) {
-			Server.deleteSocket(client);			
-		}catch(IOException e) {
-			e.printStackTrace();
-		}catch(NullPointerException e) {
-			Server.deleteSocket(client);
-		}
-	}
+    /**
+     * Socket for the Client.
+     */
+    private Socket client;
+    /**
+     * DB with pupildata.
+     */
+    private PupilDB db1;
+    /**
+     * Konstruktor to create the Thread.
+     * @param socket Clientsocket.
+     * @param pupilDB DB with pupildata.
+     */
+    public Handler(final Socket socket, final PupilDB pupilDB) {
+        this.client = socket;
+        this.db1 = pupilDB;
+    }
+    /**
+     * Manage and run the Client.
+     */
+    public void run() {
+        try {
+//            Kommunikation von Server zum Client
+            OutputStream out = client.getOutputStream();
+            PrintWriter writer = new PrintWriter(out);
+//            Kommunikation von Client zum Server
+            InputStream in = client.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String recieve = "";
+            HashMap<Integer, Integer> ranking;
+            int id = 0;
+            recieve  = reader.readLine();
+//            Solange der Client nicht offline ist.
+            while (!recieve.equals("disconnect")) {
+                if (recieve.equals("Ueberpruefe ID")) {
+//                    getID
+                    recieve = reader.readLine();
+                    id = Integer.parseInt(recieve);
+                    boolean inID = db1.isID(id);
+                    if (inID) {
+                        writer.write("true\n");
+                        writer.flush();
+                    } else {
+                        writer.write("false" + "\n");
+                        writer.flush();
+                    }
+                }
+                    if (recieve.equals("Punkte")) {
+                        Integer points = db1.getScore(id);
+                        writer.write(points.toString() + "\n");
+                        writer.flush();
+                    }
+                    if (recieve.equals("Rangliste")) {
+                        ranking = db1.getToplist();
+                        for (int i = 1; i <= ranking.size(); i++) {
+                            writer.write(ranking.get(i) + "\n");
+                            writer.flush();
+                        }
+                        writer.write(db1.getRank(id) + "\n");
+                        writer.flush();
+                    }
+                    recieve = reader.readLine();
+            }
+            System.out.println("Client: " + client.getPort() + "ist offline.");
+            Server.deleteSocket(client);
+            writer.close();
+            reader.close();
+            client.close();
+        } catch (SocketException e) {
+            Server.deleteSocket(client);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            Server.deleteSocket(client);
+        }
+    }
 }
--- Code/Server/src/Server.java
+++ Code/Server/src/Server.java
@@ -1,81 +1,94 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
 import db.PupilDB;
 /**
- * Erzeugt Server und initialisiert die DB.
+ * Create DB and start the server.
  * @author marcel
  *
  */
 public class Server {
-
-/**
- * ArrayList mit allen verbunden Clients.
- * getSocket hinzufügen.
- */
-	private static ArrayList<Socket> socket = new ArrayList<Socket>();
-
-	private PupilDB db1;
-
-/**
- * .
-*/
-	private ServerSocket server;
-
-/**
- * Port über welcher der Server laufen soll.
- */
-	static final int PORT = 1831;
-
-
-	public Server(int Port) {
-		try {
-			this.server = new ServerSocket(Port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		this.db1 = new PupilDB();
-	}
-
-	/**
-	 * Startet den Server.
-	 * @param args
-	 *            nicht weiter zubeachten
-	 */
-  public static void main(String[] args) {
-		Server serv = new Server(PORT);
-		serv.start();
-	}
-
-	/**
-	 * .
-	 */
-  private void start() {
-    ExecutorService executor = Executors.newFixedThreadPool(100);
-	try {
-			System.out.println("Server wurde gestartet");
-			Thread t = new Thread(new Commands(server));
-			System.out.println(db1.getScore(2));
-		    t.start();
-			while (true) {
-				Socket client = server.accept();
-				socket.add(client);
-				executor.execute(new Handler(client,db1));
-			}
-
-		} catch (IOException e) {
-		}
-	}
-  
-  public static ArrayList<Socket> getSocket(){
-	  return socket;
-  }
-  
-  public static void deleteSocket(Socket client) {
-	  socket.remove(client);
-  }
-
+    /**
+     * Port for the server.
+     */
+    static final int PORT = 1831;
+    /**
+     * Max number of connected clients at the same time.
+     */
+    static final int THREADNUMBER = 100;
+    /**
+     * ArrayList with all connected clients.
+     */
+    private static ArrayList<Socket> socket = new ArrayList<Socket>();
+    /**
+     * Datenbank for the pupil.
+     */
+    private PupilDB db1;
+    /**
+     * Socket for the server.
+     */
+    private ServerSocket server;
+    /**
+     * Konstruktor to create Server and DB.
+     * @param port Port for the server.
+     */
+    public Server(final int port) {
+        try {
+            this.server = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.db1 = new PupilDB();
+        System.out.println("Server is online (Port: " + port + ").");
+    }
+    /**
+     * Main-Methode.
+     * Start the server.
+     * @param args Not necessary.
+     */
+    public static void main(final String[] args) {
+        Server serv = new Server(PORT);
+        serv.start();
+    }
+    /**
+     * manage the server.
+     */
+    private void start() {
+        ExecutorService executor = Executors.newFixedThreadPool(THREADNUMBER);
+        try {
+//            starts the commands for the server.
+            Thread t = new Thread(new Commands(server));
+            t.start();
+            while (true) {
+                Socket client = server.accept();
+                socket.add(client);
+                System.out.println("Client : " + client.getInetAddress()
+                + client.getPort() + "hat sich verbunden.");
+                executor.execute(new Handler(client, db1));
+            }
+        } catch (SocketException e) {
+            } catch (IOException e) {
+            e.printStackTrace();
+            }
+    }
+    /**
+     * Returns the ArrayList with all clients.
+     * @return Liste of all connected clients.
+     */
+    public static ArrayList<Socket> getSocket() {
+        ArrayList<Socket> list = socket;
+        return list;
+    }
+    /**
+     * Delete client out of the array.
+     * @param client Clientsocket.
+     */
+    public static void deleteSocket(final Socket client) {
+        socket.remove(client);
+    }
 }
